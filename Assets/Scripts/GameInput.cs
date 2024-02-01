8cc237989ee339b11e1537bae8e4ff6b37e870af
JackyWuCoder <JackyWuCoder@gmail.com>
2024-02-01 02:48:22 -0800

    Selected Counter Visual, Singleton Pattern
    IMPORTANT**********************
    1) Extends Prefabs while working with C# events.
    2) Use Singleton Pattern to implement Counter Visual Selection logic.
    3) Refactored by creating a SetSelectedCounter function.
    4) Identified differences btw Awake() and Start(). Awake() for initializations and Start() for external references that needs some kind of initialization to occur first.

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -13,16 +13,20 @@ public class GameInput : MonoBehaviour
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
-        // Added an event listener "reference to a function" to the Interact event.
-        // Added a subscriber to the Interact event
+        // performed is a specific event we can interact with.
+        // Added an event listener (subscriber) "reference to a function" to the Interact event,
+        // of when Interact key is pressed.
         playerInputActions.Player.Interact.performed += Interact_performed;
     }
 
+    // when an InputAction is triggered, this is the publisher that fires the event.
+    // obj refers to the InputAction currently being triggered. (pressing "e" to interact)S
     private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
         // null conditional operator, code executes from left to right and when it gets to the question mark.
         // it evaluates the LHS and if it is null then the execution will stop at ? w/o errors.
         // .Invoke allows for this bc normally we cannot use this method of checking null when next line is a curly brace.
         // if the Interact event has subscribers, then fire off the event
+        // this refers to whoever is sending the event, no args is sent with this event
         OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
@@ -6,35 +7,55 @@ using static UnityEditor.PlayerSettings;
 
 public class Player : MonoBehaviour {
 
+    /*
+     * public static Player instanceField;
+     * public static Player GetInstanceField() {
+     *      return instanceField;
+     * }
+     * public static void SetInstanceField(Player instanceField) {
+     *      Player.instanceField = instanceField;
+     * }
+     */
+    // Using Properties to generate getter and setter functions.
+    /*
+    private static Player instance;
+    public static Player Instance {
+        get {
+            return instance;
+        }
+        set {
+            instance = value;
+        }
+    }
+    */
+    public static Player Instance { get; private set; }
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking;
     private Vector3 lastInteractDirection;
+    private ClearCounter selectedCounter;
 
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("There is more than one Player instance");
+        }
+        Instance = this;
+    }
     private void Start() {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero) {
-            lastInteractDirection = moveDir;
-        }
-
-        float interactDistance = 2f;
-        // raycastHit is an output filled with collision data if we hit something
-        // raycastHit only references 1 singular object, so the first object it hits
-        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
-            // if raycastHit detects a ClearCounter, then we can interact with it
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-                // Has ClearCounter
-                clearCounter.Interact();
-            }
+        if (selectedCounter != null) {
+            selectedCounter.Interact();
         }
     }
 
@@ -64,7 +85,16 @@ public class Player : MonoBehaviour {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
                 // Has ClearCounter
                 // clearCounter.Interact();
+                if (clearCounter != selectedCounter) {
+                    SetSelectedCounter(clearCounter);
+                }
+            } else {
+                // Raycast does not hit a ClearCounter
+                SetSelectedCounter(null);
             }
+        } else {
+            // Raycast does not hit any objects
+            SetSelectedCounter(null);
         }
     }
 
@@ -114,6 +144,14 @@ public class Player : MonoBehaviour {
         // Over time the player will move towards our target movement direction
         float rotateSpeed = 10f; // makes the rotation speed faster
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+            selectedCounter = selectedCounter
+        });
     }
 
 }
