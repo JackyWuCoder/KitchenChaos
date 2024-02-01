89c1e8380474ca810aebc867dbba8e245b560e5f
JackyWuCoder <JackyWuCoder@gmail.com>
2024-01-31 21:32:49 -0800

    Input System Refactor
    1) Separated GameInputs WASD from Player script logic. (Allows for Clean Code)
    2) New Input System makes it easy to add alternative input systems such as arrow keys and controller.

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,24 +4,21 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    public Vector2 GetMovementVectorNormalized() {
-        Vector2 inputVector = new Vector2(0, 0);
 
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
+    private PlayerInputActions playerInputActions;
+
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
         // normalizing the vector so that going diagonally will be same speed as WASD
         inputVector = inputVector.normalized;
+
+        Debug.Log(inputVector);
 
         return inputVector;
     }
