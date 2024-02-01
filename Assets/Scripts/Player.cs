4090780bf3eef310cdcb7b746d3037d136448c62
JackyWuCoder <JackyWuCoder@gmail.com>
2024-01-31 18:48:37 -0800

    Implemented Rotation
    Implemented Player Rotation with smoothness.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,5 +30,11 @@ public class Player : MonoBehaviour {
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         // Time.deltaTime makes the movements framerate independent; will not move faster based on higher fps
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        // rotates the player and allows it to face the direction of WASD
+        // Slerp allows the rotation to turn based on interpolations, interpolates btw a and b based on t where t i btw 0-1,
+        // Over time the player will move towards our target movement direction
+        float rotateSpeed = 10f; // makes the rotation speed faster
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
